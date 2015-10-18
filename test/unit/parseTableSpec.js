244d6048d72f3c36c3e6d9cd2c894db48938af7e
MrOrz <johnsonliang7@gmail.com>
2015-10-19 02:09:57 +0800

    [parseTable] shorter property assertions in spec.

--- test/unit/parseTableSpec.js
+++ test/unit/parseTableSpec.js
@@ -18,25 +18,25 @@ describe('htparser.parseTable', () => {
     it('should parse authors, ids and dates', () => {
       expect(commentMap).to.be.an('object');
       expect(commentMap).to.contain.all.keys('77', '41', '75');
-      expect(commentMap['77']).to.have.property('author').and.equals('Johnson Liang');
-      expect(commentMap['77']).to.have.property('id').and.equals('77');
+      expect(commentMap['77']).to.have.property('author', 'Johnson Liang');
+      expect(commentMap['77']).to.have.property('id', '77');
       expect(commentMap['77']).to.have.property('date').and.eqls(new Date("2015-10-03T14:01:23Z"));
     });
 
     it('should give correct types for each comment', () => {
       // Single-paragraph comments. Type = Note
-      expect(commentMap['102']).to.have.property('type').and.equal(COMMENTS.NOTE);
-      expect(commentMap['102']).to.have.property('text').and.equal("據官網：成立類似「人口、健康及社會保障研究中心」建置長期照顧資料以利整合各縣市資訊系統。(1)非營利、營利、準營利服務單位均應納入管理與監督(政府財源應優先用於擴大非營利組織所提供的長期照顧服務)。(2)發展更精確的需求評估工具，以利實現以需求評估結果作為服務的依據，避免資源浪費。");
+      expect(commentMap['102']).to.have.property('type', COMMENTS.NOTE);
+      expect(commentMap['102']).to.have.property('text', "據官網：成立類似「人口、健康及社會保障研究中心」建置長期照顧資料以利整合各縣市資訊系統。(1)非營利、營利、準營利服務單位均應納入管理與監督(政府財源應優先用於擴大非營利組織所提供的長期照顧服務)。(2)發展更精確的需求評估工具，以利實現以需求評估結果作為服務的依據，避免資源浪費。");
 
       // Multi-paragraph comments. Type = Note
-      expect(commentMap['70']).to.have.property('type').and.equal(COMMENTS.NOTE);
-      expect(commentMap['70']).to.have.property('text').and.equal("具體作法：\n1.擴大政治參與機會\n2.建立城鄉產業連結\n3.保障居住權\n4.維護接受語言和文化教育的權利");
+      expect(commentMap['70']).to.have.property('type', COMMENTS.NOTE);
+      expect(commentMap['70']).to.have.property('text', "具體作法：\n1.擴大政治參與機會\n2.建立城鄉產業連結\n3.保障居住權\n4.維護接受語言和文化教育的權利");
 
       // Type = Controlversial reference
-      expect(commentMap['23']).to.have.property('type').and.equals(COMMENTS.REF_CONTROVERSIAL);
+      expect(commentMap['23']).to.have.property('type', COMMENTS.REF_CONTROVERSIAL);
 
       // Mistyped type
-      expect(commentMap['26']).to.have.property('type').and.equals(COMMENTS.OTHER);
+      expect(commentMap['26']).to.have.property('type', COMMENTS.OTHER);
     });
   });
 
