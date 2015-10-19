30e1c054e7c5688e8dee1ecde4bfdad2e9aae594
MrOrz <johnsonliang7@gmail.com>
2015-10-19 10:15:58 +0800

    [index] Expose components for developers to make ‘instanceof’ checks.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import {DEFAULTS, ERRORS, COMMENTS} from './constants';
 import fetchConfig from './fetchConfig';
 import fetchDoc from './fetchDoc';
 import parseTable from './parseTable';
+import components from './components'; // For developers to make 'instanceof' check.
 
-export {DEFAULTS, ERRORS, COMMENTS, fetchConfig, fetchDoc, parseTable};
+export {DEFAULTS, ERRORS, COMMENTS, fetchConfig, fetchDoc, parseTable, components};
 
 /* The main interface */
 export default async function hacktablParser (etherCalcId) {
